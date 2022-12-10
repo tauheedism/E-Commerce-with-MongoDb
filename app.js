4b1a12d932677d736641f1bbaf3eb94d66d15204
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2022-12-10 14:03:41 +0530

    Update Cart Items to Quantity

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('63942f4539a717ffe7fab4ce')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
